ccc7a6fb2ec73109db470c2b0ce9deaceb409467
lehuy08012001 <lehuy0801ht@gmail.com>
2022-02-27 14:59:27 +0700

    push9

--- server.js
+++ server.js
@@ -54,12 +54,13 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+var distDir = __dirname + "/dist/";
+
+app.use(express.static(distDir));
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // post
 const POST = process.env.POST || 3000;
