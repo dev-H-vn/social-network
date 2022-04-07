fcf96f6145511077ea751bb6b872780ebe1bf451
dev-H-vn <huylee.dev.vn@gmail.com>
2022-04-07 22:07:43 +0700

    .

--- server.js
+++ server.js
@@ -60,9 +60,9 @@ mongoose.connect(
   }
 );
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
 
-app.get("*", function (req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client", "build", "index.html"));
 });
 
